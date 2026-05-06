27a57a16067dc3d5bb94fb890aefcbe92fa7257d
mohamedZouari1 <mohamedzouari276@gmail.com>
2026-05-06 18:21:06 +0100

    Add database URL support for PostgreSQL

--- SmartCampusBackend/smartcampus/settings.py
+++ SmartCampusBackend/smartcampus/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,10 +56,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'smartcampus.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
