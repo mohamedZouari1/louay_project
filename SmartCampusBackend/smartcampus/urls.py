f1d4ffe4dc08587984d8cb06ac05418978fd9c37
mohamedZouari1 <mohamedzouari276@gmail.com>
2026-05-06 19:11:18 +0100

    Force serve media files in production for demo

--- SmartCampusBackend/smartcampus/urls.py
+++ SmartCampusBackend/smartcampus/urls.py
@@ -3,7 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.urls import re_path
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
